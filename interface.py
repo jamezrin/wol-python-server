faacc26d1a4698c40b2575a8ee9e64ed035ab65f
Jaime Martinez Rincon <mrjaime1999@gmail.com>
2017-03-09 10:59:03 +0100

    Decided to use yaml files instead of ini

--- interface.py
+++ interface.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python
 
+import yaml
+
+APP_ADDRESS = "0.0.0.0"
+APP_PORT = 5080
+
 def main():
+    print yaml.load(open('devices.conf'))
     print 'This has not been implemented yet.'
 
 
